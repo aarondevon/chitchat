a51d4876ac40faabbaf249fcf8191e26ea856dba
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-02 10:42:09 -0500

    Add some initial files

---
+++ API/client/src/components/ChatPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function chatPage() {
+
+  return (
+    <div id="chat-page-container">
+      <div id="chat-page-header">
+        <div>ChitChat</div>
+        <div>
+          <button id="log-out">Log Out</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default chatPage;
---
+++ API/client/src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function HomePage() {
+
+  return (
+    <div id="home-page-container">
+      <div id="login-container">
+        <h1>ChitChat</h1>
+        <button> Log In</button>
+        <div>
+          <a href="#">Sign Up</a>
+          <a href="#">Demo</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
