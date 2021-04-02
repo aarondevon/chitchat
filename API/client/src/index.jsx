9a2dc0125eb11513b751f3ec789e14b8bf67ca30
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-02 14:26:04 -0500

    silence eslint warnings and errors

--- API/client/src/index.jsx
+++ API/client/src/index.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
