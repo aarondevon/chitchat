f7feb52672b86d2c530dc7294325bd6d970f6bd1
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-03 08:58:27 -0500

    Create user list component

---
+++ API/client/src/components/UserList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function UserList() {
+  return (
+    <div>
+      <h3>Active Users</h3>
+      <ul className="list-group border">
+        <li className="list-group-item border-0">Abigail S</li>
+        <li className="list-group-item border-0">John C</li>
+        <li className="list-group-item border-0">Aaron S</li>
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
