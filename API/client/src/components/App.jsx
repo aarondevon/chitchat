22e2abec0e2756637aedcf1bdf9a51392966f95f
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-03 08:57:22 -0500

    Test chatPage

--- API/client/src/components/App.jsx
+++ API/client/src/components/App.jsx
@@ -1,11 +1,14 @@
-import HomePage from './HomePage';
+import React from 'react';
+// import HomePage from './HomePage';
+import ChatPage from './ChatPage';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <ChatPage />
     </div>
   );
 }
