579783ac0dc032eb2b8bb570f639d920129e9ca4
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-08 10:17:27 -0500

    Add message model

---
+++ CoreLibrary/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Models
+{
+    public class MessageModel
+    {
+        //public int Id { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
+    }
+}
