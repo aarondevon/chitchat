acbcb6981018d3f1f364f5d1bb492f8e0cc26f72
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-08 14:56:54 -0500

    Set up dummy user data

---
+++ CoreLibrary/Data/GetDummyUsers.cs
@@ -0,0 +1,26 @@
+﻿using CoreLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Data
+{
+    public class GetDummyUsers : IGetAllUsers
+    {
+        private readonly List<UserModel> _user = new List<UserModel>();
+
+        public GetDummyUsers()
+        {
+            _user.Add(new UserModel { Id = 1, Username = "Aaron S." });
+            _user.Add(new UserModel { Id = 2, Username = "Abigail S." });
+            _user.Add(new UserModel { Id = 3, Username = "John C." });
+        }
+
+        public List<UserModel> GetAllUsers()
+        {
+            return _user;
+        }
+    }
+}
