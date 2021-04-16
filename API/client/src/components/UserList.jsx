b12b5d4b165eee248e46893855ae6c83adfafdba
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-16 11:48:35 -0500

    Display users

--- API/client/src/components/UserList.jsx
+++ API/client/src/components/UserList.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable arrow-body-style */
 import React from 'react';
 
-function UserList() {
+function UserList(props) {
+  const displayUsers = () => {
+    return props.users.map((user) => {
+      return <li className="list-group-item border-0">{user.username}</li>;
+    });
+  };
+
   return (
     <div>
       <h3>Active Users</h3>
       <ul id="user-list" className="list-group border">
-        <li className="list-group-item border-0">Abigail S</li>
-        <li className="list-group-item border-0">John C</li>
-        <li className="list-group-item border-0">Aaron S</li>
+        { displayUsers() }
       </ul>
     </div>
   );
