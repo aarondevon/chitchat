40f97b104ae0903cc422213c458c0292d9b1286e
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-23 11:42:19 -0500

    Wrap app in browser router

--- API/client/src/index.jsx
+++ API/client/src/index.jsx
@@ -4,12 +4,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
