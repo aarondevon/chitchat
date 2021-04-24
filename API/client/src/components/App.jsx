9f6570f5a9b4ce660e42380d78552c3193fab667
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-24 09:13:04 -0500

    Load  Homepage

--- API/client/src/components/App.jsx
+++ API/client/src/components/App.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
-// import HomePage from './HomePage';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import AuthService from '../services/auth.service';
+
+import Login from './Login';
+import Register from './Register';
+import HomePage from './HomePage';
 import ChatPage from './ChatPage';
 // import logo from './logo.svg';
 // import './App.css';
@@ -7,8 +14,8 @@ import ChatPage from './ChatPage';
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
-      <ChatPage />
+      <HomePage />
+      {/* <ChatPage /> */}
     </div>
   );
 }
