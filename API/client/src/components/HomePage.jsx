5afa68700945fd881405ac0b609145d7d94d2c20
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-24 09:14:03 -0500

    Add some routing

--- API/client/src/components/HomePage.jsx
+++ API/client/src/components/HomePage.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import Register from './Register';
+import Login from './Login';
 
 function HomePage() {
   return (
     <div id="home-page-container">
       <div id="login-container">
         <h1>ChitChat</h1>
-        <button> Log In</button>
+        {/* <button> Log In</button> */}
+        <Link to="/login"> Log In</Link>
         <div>
-          <a href="#">Sign Up</a>
-          <a href="#">Demo</a>
+          <Link to="/register">Sign Up </Link>
+          {/* <a href="#">Demo</a> */}
         </div>
       </div>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </div>
   );
 }
