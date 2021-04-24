3997efe9e4c14b705e1d9600be9d4988a7664872
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-24 09:21:18 -0500

    Start building logic for registering a user

---
+++ CoreLibrary/Data/RegisterUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Data
+{
+    class RegisterUser : IRegisterUser
+    {
+        void IRegisterUser.RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
