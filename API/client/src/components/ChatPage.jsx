6f4b02ba4454afd1b1f671fed3b6506252ffdedd
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-29 15:58:42 -0500

    Use signalR with post request

--- API/client/src/components/ChatPage.jsx
+++ API/client/src/components/ChatPage.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import * as signalR from '@microsoft/signalr';
 import { useHistory } from 'react-router-dom';
 import AuthService from '../services/auth.service';
 import AuthHeader from '../services/auth-header';
@@ -10,6 +12,7 @@ import ChatInput from './ChatInput';
 
 function ChatPage() {
   const history = useHistory();
+  const latestChat = useRef(null);
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
 
@@ -26,19 +29,33 @@ function ChatPage() {
     setMessages(responseMessages);
   };
 
-  const addNewMessageToState = async (event, message) => {
+  const sendMessage = async (event, message) => {
     event.preventDefault();
+    const chatMessage = {
+      username: currentUser.Username,
+      message,
+    };
 
-    await axios({
-      method: 'post',
-      url: 'api/messages',
-      data: {
-        userId: currentUser.userId,
-        message,
-      },
-    });
-    const newMessage = message;
-    setMessages([...messages, { message: newMessage, user: { username: currentUser.Username } }]);
+    try {
+      await axios({
+        method: 'post',
+        headers: AuthHeader(),
+        url: 'api/messages',
+        data: {
+          message,
+          user: {
+            id: currentUser.userId,
+            userName: currentUser.Username,
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+    // const newMessage = message;
+    // eslint-disable-next-line max-len
+    // setMessages([...messages, { message: newMessage, user: { username: currentUser.Username } }]);
   };
 
   const getUsers = async () => {
@@ -57,14 +74,38 @@ function ChatPage() {
     history.push('/');
   };
 
+  latestChat.current = messages;
   useEffect(() => {
     setMessagesToState();
     setUsersToState();
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-  }, [messages]);
+  // }, [messages]);
+
+  useEffect(() => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl('/message')
+      .withAutomaticReconnect()
+      .build();
+
+    connection.start()
+      .then((result) => {
+        console.log('Connected!');
+
+        connection.on('sendToReact', (message) => {
+          const newMessage = message;
+          // eslint-disable-next-line max-len
+          const updatedChat = [...latestChat.current, {
+            message: message.message,
+            user: { id: message.user.id, username: message.user.username },
+          }];
+          setMessages(updatedChat);
+        });
+      })
+      .catch((e) => console.log('Connection failed: ', e));
+  }, []);
 
   return (
     <div id="chat-page-container" className="container">
@@ -88,7 +129,7 @@ function ChatPage() {
       </div>
 
       <div>
-        <ChatInput saveNewMessage={addNewMessageToState} />
+        <ChatInput saveNewMessage={sendMessage} />
       </div>
 
     </div>
