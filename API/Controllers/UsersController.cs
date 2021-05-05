716b512a039535418569727bd071dead75d2f1e4
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-05-05 13:24:37 -0500

    Update routes and add authorization to get users

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,17 +5,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JWT;
+using JWT.Serializers;
+using JWT.Algorithms;
+using JWT.Exceptions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
         private IUserService _users;
         private IRegisterUserService _registerUser;
+        //private readonly string secret = Environment.GetEnvironmentVariable("SECRET");
+        string secret = "9z3j%mCFt3XZEi2UGJ3KBTaxgx&ZQEjQNJz4Su95x%ucaWuv#L%DFCoZAz6@";
 
         public UsersController(IUserService users, IRegisterUserService registerUser)
         {
@@ -24,13 +30,37 @@ namespace API.Controllers
         }
 
         // GET: api/<UserController>
+        [Route("api/users")]
         [HttpGet]
-        public IEnumerable<UserModel> Get()
+        public IActionResult Get()
         {
-            return _users.GetAllUsers();
+            Request.Headers.TryGetValue("Authorization", out var token);
+
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                var provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+                var json = decoder.Decode(token, secret, verify: true);
+                return Ok(_users.GetAllUsers());
+            }
+            catch (TokenExpiredException)
+            {
+                return StatusCode(401, "Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                return StatusCode(401, "Token has invalid signature");
+            }
+            return StatusCode(500, "Server error");
         }
 
         // GET api/<UserController>/5
+        [Route("api/users")]
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -52,6 +82,7 @@ namespace API.Controllers
         }
 
         // POST api/<UserController>
+        [Route("api/register")]
         [HttpPost]
         public void Post([FromBody] RegisterLoginUserModel user)
         {
