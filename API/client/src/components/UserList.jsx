1788ba63a2014d41964d55ae4d6e69a84b062bc9
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-05-05 20:18:11 -0500

    add drop right menu and key to user list

--- API/client/src/components/UserList.jsx
+++ API/client/src/components/UserList.jsx
@@ -1,20 +1,36 @@
+/* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import {
+  DropdownButton,
+  Dropdown,
+} from 'react-bootstrap';
 
 function UserList(props) {
   const displayUsers = () => {
     return props.users.map((user) => {
-      return <li className="list-group-item border-0">{user.username}</li>;
+      return <Dropdown className="list-group-item border-0" key={user.id}>{user.username}</Dropdown>;
     });
   };
 
   return (
     <div>
-      <h3>Active Users</h3>
-      <ul id="user-list" className="list-group border">
+      <h3 id="user-title">Users</h3>
+      <div>
+        <ul id="user-list" className="list-group border">
+          { displayUsers() }
+        </ul>
+      </div>
+
+      <DropdownButton
+        id="user-drop-right"
+        drop="right"
+        variant="dark"
+        title="Users"
+      >
         { displayUsers() }
-      </ul>
+      </DropdownButton>
     </div>
   );
 }
